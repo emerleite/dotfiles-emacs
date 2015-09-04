629884716fd43cb075b72f7aac05724cf2a71936
Emerson Macedo <emerleite@gmail.com>
2015-09-04 16:39:46 -0300

    Elixir mode

---
+++ modules/elixir.el
@@ -0,0 +1,7 @@
+(add-to-list 'el-get-sources
+	     '(:name elixir
+		     :type github
+		     :pkgname "elixir-lang/emacs-elixir"
+		     :description "Elixir language's major mode for Emacs"
+		     :website "http://github.com/elixir-lang/emacs-elixir"))
+

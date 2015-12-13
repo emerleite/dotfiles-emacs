ccea1af66706d8446f1ae5dc2a28066c2969e99a
Emerson Macedo <emerleite@gmail.com>
2015-12-13 21:37:02 -0200

    Replaced js3-mode with js2-mode. Also added JSX support

--- modules/javascript.el
+++ modules/javascript.el
@@ -1,3 +1,9 @@
 (add-to-list 'el-get-sources
-	     '(:name js3-mode
-		     :after (setq js3-highlight-external-variables nil)))
+	     '(:name js2-mode
+                     :after (progn
+                              (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+                              (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
+                              (add-to-list 'auto-mode-alist '("\\.json?\\'" . js2-jsx-mode))
+                              (setq-default js2-basic-offset 2)
+                              (setq js2-strict-missing-semi-warning nil)
+                              (setq js2-missing-semi-one-line-override nil))))
--- modules/markup.el
+++ modules/markup.el
@@ -8,4 +8,8 @@
 	     '(:name web-mode
 		     :after (progn
                               (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+                              (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+                              (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+                              (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+                              (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
                               (add-hook 'sgml-mode-hook 'web-mode))))

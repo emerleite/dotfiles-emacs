57e82edeb56cc67ed057b7ebf806801aa94b6c24
Emerson Macedo <emerleite@gmail.com>
2019-04-04 14:38:15 -0300

    Added Kotlin mode

---
+++ modules/kotlin.el
@@ -0,0 +1,5 @@
+(add-to-list 'el-get-sources
+	     '(:name kotlin-mode
+		     :type github
+		     :description "Emacs major mode for editing Kotlin source code"
+		     :pkgname "Emacs-Kotlin-Mode-Maintainers/kotlin-mode"))

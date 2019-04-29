196f72f08f717e15324ac6004b79208beee7acad
Emerson Macedo <emerleite@gmail.com>
2019-04-29 14:09:01 -0300

    Go formatter when save

--- modules/go.el
+++ modules/go.el
@@ -2,4 +2,6 @@
 	     '(:name go-mode
 		     :description "Major mode for the Go programming language"
 		     :type github
-		     :pkgname "dominikh/go-mode.el"))
+		     :pkgname "dominikh/go-mode.el"
+                     :after (progn
+                              (add-hook 'before-save-hook #'gofmt-before-save))))

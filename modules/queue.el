4f546526c7f141f15cd5fed221f0e241aa75ff4c
Emerson Macedo <emerson@Emersons-MacBook-Pro.local>
2019-07-22 17:34:40 -0300

    Added queue module to work with emacs 26

---
+++ modules/queue.el
@@ -0,0 +1,6 @@
+(add-to-list 'el-get-sources
+             '(:name queue
+                     :type github
+                     :pkgname "emacsmirror/queue"
+                     :description "Queue data structure"
+                     :website "https://github.com/emacsmirror/queue"))
